ba528de3be25ae1e9c8ed2d0f7b730ec2a4e165f
Snegha-M <99195190+Snegha-M@users.noreply.github.com>
2022-08-18 11:33:24 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Student {
+	private String name;
+	private int id;
+	private int marks;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public int getMarks() {
+			return marks;
+		}
+
+		public void setMarks(int marks) {
+			this.marks = marks;
+		}
+
+		public Student(String name, int id, int marks) {
+			super();
+			this.name = name;
+			this.id = id;
+			this.marks = marks;
+		}
+
+		
+		
+
+}
+
+
